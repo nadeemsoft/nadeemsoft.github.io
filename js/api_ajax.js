d4565e40478059dd4b7815d3d9a878d67fc6a107
nadeemsoft <nadeemashrafi38@gmail.com>
2020-03-24 03:24:02 +0530

    implemented api for header

--- js/api_ajax.js
+++ js/api_ajax.js
@@ -1,4 +1,4 @@
-
+//banner ajax call
 $.ajax({
     type:"get",
     url: "http://mgcollege.pythonanywhere.com/apiv1/banners", 
@@ -12,9 +12,25 @@ $.ajax({
             imgList +="<li><img src="+response[i].image+"></li>";
         }
         document.getElementById("fullscreen-slider").innerHTML = imgList;
+        setTimeout(function(){
+            $("#fullscreen-slider").slider();
+        }, 1000);
     }
 });
+//banner ajax call
 
-setTimeout(function(){
-    $("#fullscreen-slider").slider();
-}, 3000);  
+$.ajax({
+    type:"get",
+    url:"http://mgcollege.pythonanywhere.com/apiv1/organisation_details/",
+    dataType:"json",
+    success:function(response){
+    console.log(response[0].logo);
+    document.getElementById("logo").src= response[0].logo;
+    document.getElementById("orgName").innerHTML = response[0].organisation_name;
+    document.getElementById("contactNo").innerHTML = response[0].contact_no;
+    document.getElementById("emailIdTop").innerHTML = response[0].email_id;
+    document.getElementById("facebookUrl").href = response[0].facebook_link;
+    document.getElementById("twitterUrl").href = response[0].twitter_link;
+    document.getElementById("instagramUrl").href = response[0].instagram_link;
+    }
+});
